641de0b6a7bf849ba36a55b27c8c3e9d2c39bce0
Nitish Mathur <nitishmathurmain@gmail.com>
2025-10-15 03:21:11 +0530

    added asio

---
+++ code/GameServer/GameServer.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <asio.hpp>  // standalone Asio include
+
+using asio::ip::tcp;
+
+int main() {
+    try {
+        asio::io_context io;
+        tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 8080));
+
+        std::cout << "Server started. Listening on port 8080..." << std::endl;
+
+        for (;;) {
+            tcp::socket socket(io);
+            acceptor.accept(socket);
+
+            std::string msg = "Hello from GameServer!\n";
+            asio::write(socket, asio::buffer(msg));
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
+
+    return 0;
+}
